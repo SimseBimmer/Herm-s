3e302cadbd9360d7e0fcb6405cd0a72875d1c54e
SimseBimmer <simonsej1710@gmail.com>
2024-05-23 17:10:02 +0200

    123

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,2 +1,22 @@
-// JavaScript
-console.log('hello wrld')
+// #region VeryTopNavigation fixed
+document.addEventListener('DOMContentLoaded', function() {
+    const nav = document.querySelector('.fixed-Navigation');
+    const topNavHeight = document.querySelector('.veryTop-Navigation').offsetHeight;
+    const navOffsetTop = nav.offsetTop;
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > navOffsetTop - topNavHeight) {
+            nav.classList.add('fixed');
+            document.body.style.paddingTop = nav.offsetHeight + 'px'; // gør mere smooth
+        } else {
+            nav.classList.remove('fixed');
+            document.body.style.paddingTop = '0';
+        }
+    });
+});
+// #endregion VeryTopNavigation fixed
+// #region search
+
+//#endregion search
+
+
